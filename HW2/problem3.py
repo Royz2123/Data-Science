355fe2cdb6e1b42a4418d7a7847fd44a1791e622
Royz2123 <royzohar25@gmail.com>
2018-12-06 12:31:23 +0200

    Added Question3 part 4

--- HW2/problem3.py
+++ HW2/problem3.py
@@ -77,4 +77,42 @@ def question_c():
     util.write_list(data1, "problem3_outputs/question_c/data1.txt")
     util.write_list(data2, "problem3_outputs/question_c/data2.txt")    
         
-question_c()
+        
+def question_d():
+    # In order to generate a circle, we will sample two parameters
+    # R and theta, and from them
+    obj_pnts = int(TOTAL_POINTS / 2)
+    plt_obj_pnts = int(POINTS_PER_PLOT / 2)
+    
+    # Sample the inner circle
+    circle_r_data = np.random.uniform(0, 0.5, (obj_pnts, 1))
+    circle_t_data = np.random.uniform(0, 2 * np.pi, (obj_pnts, 1))
+    
+    # Sample the outer ring
+    ring_r_data = np.random.uniform(1.5, 1.75, (obj_pnts, 1))
+    ring_t_data = np.random.uniform(0, 2 * np.pi, (obj_pnts, 1))
+    
+    # concatenate each plot
+    r_data1 = np.concatenate((circle_r_data[:plt_obj_pnts], ring_r_data[:plt_obj_pnts]), axis=0)  
+    r_data2 = np.concatenate((circle_r_data[plt_obj_pnts:], ring_r_data[plt_obj_pnts:]), axis=0)  
+    t_data1 = np.concatenate((circle_t_data[:plt_obj_pnts], ring_t_data[:plt_obj_pnts]), axis=0) 
+    t_data2 = np.concatenate((circle_t_data[plt_obj_pnts:], ring_t_data[plt_obj_pnts:]), axis=0) 
+    polar_data1 = np.concatenate((r_data1, t_data1), axis=1)
+    polar_data2 = np.concatenate((r_data2, t_data2), axis=1)
+        
+    # turn from polar to cartesian
+    total_data = []
+    for data in (polar_data1, polar_data2):
+        total_data.append([util.pol2cart(point[0], point[1]) for point in data])
+    data1, data2 = np.array(total_data[0]), np.array(total_data[1]) 
+    
+    # PLOT the outputs
+    for data in (data1, data2):
+        util.plot_in_R2(data.T[0], data.T[1], "Circle inside a Ring")
+
+    # save the points
+    util.write_list(data1, "problem3_outputs/question_d/data1.txt")
+    util.write_list(data2, "problem3_outputs/question_d/data2.txt")
+        
+        
+question_d()
--- HW2/util.py
+++ HW2/util.py
@@ -1,8 +1,19 @@
 import json
 import matplotlib.pyplot as plt
+import numpy as np
 
 DEFAULT_LINKS_FILE = "project_links.txt"
 DEFAULT_OUTPUT_FILE = "json_data.json"
+
+def cart2pol(x, y):
+    rho = np.sqrt(x**2 + y**2)
+    phi = np.arctan2(y, x)
+    return(rho, phi)
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return(x, y)
 
 def write_list(lst, filename=DEFAULT_LINKS_FILE):
     with open(filename, 'w') as fp:
